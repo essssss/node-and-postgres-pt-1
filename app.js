94a9d93fa8fd11b4839f87fe5cbe61e8095117ff
Steven <srmcnamara7@gmail.com>
2023-08-14 15:47:21 -0400

    doing

--- app.js
+++ app.js
@@ -1,31 +1,36 @@
 /** BizTime express application. */
 
-
 const express = require("express");
 
 const app = express();
-const ExpressError = require("./expressError")
+const ExpressError = require("./expressError");
 
 app.use(express.json());
-
-
+const companyRoutes = require("./routes/companies");
+app.use("/companies", companyRoutes);
+const invoiceRoutes = require("./routes/invoices");
+app.use("/invoices", invoiceRoutes);
 /** 404 handler */
 
-app.use(function(req, res, next) {
-  const err = new ExpressError("Not Found", 404);
-  return next(err);
+app.use(function (req, res, next) {
+    const err = new ExpressError("Not Found", 404);
+
+    // pass err to the next middleware
+    return next(err);
 });
 
 /** general error handler */
 
-app.use((err, req, res, next) => {
-  res.status(err.status || 500);
+app.use(function (err, req, res, next) {
+    // the default status is 500 Internal Server Error
+    let status = err.status || 500;
 
-  return res.json({
-    error: err,
-    message: err.message
-  });
+    // set the status and alert the user
+    return res.status(status).json({
+        error: {
+            message: err.message,
+            status: status,
+        },
+    });
 });
-
-
 module.exports = app;
